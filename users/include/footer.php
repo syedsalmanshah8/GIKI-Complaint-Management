8716f3e5545045e841ea6fb6174fef3f2bb85626
Syed Salman Shah <79438971+syedsalmanshah8@users.noreply.github.com>
2021-08-18 13:29:44 +0500

    Add files via upload

---
+++ users/include/footer.php
@@ -0,0 +1,8 @@
+ <footer class="footer">
+          <div class="text-center">
+              &copy; 2020 GIKI All rights reserved.
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
---
+++ users/include/header.php
@@ -0,0 +1,13 @@
+ <header class="header">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <!--logo start-->
+            <a href="index.html" class="logo"><b>GIKI Portal</b></a>
+                
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
